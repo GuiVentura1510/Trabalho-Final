674fe7cce47cd13dda531d09bddd4c51bbdcaf19
Guilherme <guilorenziventura@gmail.com>
2024-11-26 22:13:58 -0300

    Transformei em botões os componentes da Barra de Navegação

--- cine-orc/src/pages/Home/home.jsx
+++ cine-orc/src/pages/Home/home.jsx
@@ -3,10 +3,18 @@ function Home() {
 
     return (
         <div>
-            <header>Home</header>
+            <header>
+                <h3 className='titulo'>Cine Orc</h3>
+                <nav className='tipos-filme'>
+                    <button>Em Cartaz</button>
+                    <button>Maior Nota</button>
+                </nav>
+                <div className="acessar-conta">
+                    <button>Criar Conta</button>
+                    <button>Entrar</button>
+                </div>
+            </header>
         </div>
-
-
     )
 }
 
