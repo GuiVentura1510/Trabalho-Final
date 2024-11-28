f646a24ab184ecf8f0ad8b17c767ce4b3e601ff1
Galdino <pedro.wollmann@gmail.com>
2024-11-28 18:07:09 -0300

    Titulo dos filmes aparecem na pagina teste

--- cine-orc/src/pages/Teste/teste.jsx
+++ cine-orc/src/pages/Teste/teste.jsx
@@ -13,43 +13,16 @@ function Teste() {
     const getTopRatedMovies = async(url) =>{
         const res = await fetch(url)
         const data = await res.json()
-        console.log(data)
+        setTopMovies(data.results)
     }
     useEffect(() => {
         const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
-        console.log(topRatedUrl)
+        getTopRatedMovies(topRatedUrl)
     },[])
 
     return (
         <div>
-            <header>
-                <h3 className='titulo'>Cine Orc</h3>
-                <nav className='tipos-filme'>
-                    <button>Em Cartaz</button>
-                    <button>Maior Nota</button>
-                </nav>
-                <div className="acessar-conta">
-                    <Link to="/Registrar" className='link'>
-                        <button>Criar Conta</button>
-                    </Link>
-                    <Link to="Login" className='link'>
-                        <button>Entrar</button>
-                    </Link>
-                </div>
-            </header>
-            <div className='search-bar'>
-                <input
-                    type='search'
-                    placeholder='Digite o nome do filme'
-                    value={filme}
-                    onChange={(e) => setFilme(e.target.value)}
-                ></input>
-            </div>
-            <div className='filtrar-filmes'>
-                <button>Sem filtro</button>
-                <button>Por Ordem Alfabética</button>
-                <button>Por Ordem de Lançamento</button>
-            </div>
+                {topMovies && topMovies.map((movie)=><p>{movie.title}</p>)}
         </div>
     )
 }
