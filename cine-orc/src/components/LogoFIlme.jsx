197ca36801d53b5050cfdb1e7a4244e3e5c0ffc6
Galdino <pedro.wollmann@gmail.com>
2024-11-28 18:32:35 -0300

    criei um componente para o poster do filme ser mostrado na home

---
+++ cine-orc/src/components/LogoFIlme.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom"
+
+const imageUrl =import.meta.env.VITE_IMG
+
+const LogoFilme = ({movie, showLink=true}) => {
+  return (
+    <div className="logo-filme">
+        <img src={imageUrl + movie.poster_path} alt={movie.title} />
+        <h2>{movie.title}</h2>
+        {showLink && <Link to ={`/FIlme/${movie.id}`}>Detalhes</Link>}
+    </div>
+  )
+}
+
+export default LogoFilme
