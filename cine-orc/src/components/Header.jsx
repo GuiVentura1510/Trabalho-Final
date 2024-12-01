81d5d28613afb667880d24816d31e9772f8a96a7
Galdino <pedro.wollmann@gmail.com>
2024-12-01 10:00:41 -0300

    Componente header agora verifica se esta logado

--- cine-orc/src/components/Header.jsx
+++ cine-orc/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Header.css'
-function Header({redirect}) {
+function Header({redirect, logado, username}) {
   return (
     <div>
       <header>
@@ -10,12 +10,25 @@ function Header({redirect}) {
           <button>Em Cartaz</button>
           <button>Maior Nota</button>
           <div className="acessar-conta">
+            {logado ?(
+              <div className='buttonL'>
+                <Link to="/Conta" className='link'>
+                  <button className='bem-vindo'> {username.split(' ')[0]}</button>
+                </Link>
+                <Link to='/'>
+                  <button className='sair'> Sair</button>
+                </Link>
+              </div>
+            ):(
+              <div>
             <Link to="/Registrar" className='link'>
                 <button>Criar Conta</button>
             </Link>
             <Link to="Login" className='link'>
                 <button>Entrar</button>
             </Link>
+            </div>
+            )}
           </div>
         </nav>
       </header>
