2bf0b043bc69cf5f20439db0559261a7406d3a2d
Galdino <pedro.wollmann@gmail.com>
2024-11-30 21:14:33 -0300

    duvida pra tirar

--- cine-orc/src/components/SearchBar.jsx
+++ cine-orc/src/components/SearchBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './SearchBar.css'
-function SearchBar() {
+function SearchBar(pesquisar) {
     const [filme, setFilme] = useState('');
   return (
     <div className='search-bar'>
+      <form >
         <input
             type='search'
             placeholder='Digite o nome do filme'
             value={filme}
-            onChange={(e) => setFilme(e.target.value)}
+            onChange={(e) => setFilme(e.target.value)} //ver com o guilerme oq isso significa
         ></input>
+      </form>
     </div>
   )
 }
