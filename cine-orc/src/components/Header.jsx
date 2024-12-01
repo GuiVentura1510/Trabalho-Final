247a427bcd5a2c52cbb077d77696d24f6e5dbc32
Galdino <pedro.wollmann@gmail.com>
2024-11-30 21:13:54 -0300

    Corrigindo o codigo da header

--- cine-orc/src/components/Header.jsx
+++ cine-orc/src/components/Header.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Header.css'
-function Header() {
+function Header({redirect}) {
   return (
     <div>
       <header>
-        <h3 className='titulo'>Cine Orc</h3>
         <nav className='tipos-filme'>
-            <button>Em Cartaz</button>
-            <button>Maior Nota</button>
-        </nav>
-        <div className="acessar-conta">
+        <button onClick={redirect} className="titulo">Cine Orc</button>
+          <button>Em Cartaz</button>
+          <button>Maior Nota</button>
+          <div className="acessar-conta">
             <Link to="/Registrar" className='link'>
                 <button>Criar Conta</button>
             </Link>
             <Link to="Login" className='link'>
                 <button>Entrar</button>
             </Link>
-        </div>
-        </header>
+          </div>
+        </nav>
+      </header>
     </div>
   )
 }
