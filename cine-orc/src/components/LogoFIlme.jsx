51ec941664aebdf0961c04a788748753787fd57d
Galdino <pedro.wollmann@gmail.com>
2024-12-02 17:27:12 -0300

    retirei o link detalhes e fiz a funcao de clicar da imagem

--- cine-orc/src/components/LogoFIlme.jsx
+++ cine-orc/src/components/LogoFIlme.jsx
@@ -1,14 +1,19 @@
-import {Link} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 import './LogoFilme.css'
 
 const imageUrl =import.meta.env.VITE_IMG
 
 const LogoFilme = ({movie, showLink=true}) => {
+  const navigate = useNavigate()
+
+  const handleImageClick= () =>{
+    navigate(`/Filme/${movie.id}`)
+  }
+
   return (
     <div className="logo-filme">
-        <img src={imageUrl + movie.poster_path} alt={movie.title} />
+        <img src={imageUrl + movie.poster_path} alt={movie.title} onClick={handleImageClick}  />
         <h2>{movie.title}</h2>
-        {showLink && <Link to ={`/Filme/${movie.id}`}>Detalhes</Link>}
     </div>
   )
 }
