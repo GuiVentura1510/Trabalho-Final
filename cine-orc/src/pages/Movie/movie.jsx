be12471f959aeca924f1ed60cdedeb0ab8609c6e
Galdino <pedro.wollmann@gmail.com>
2024-12-02 08:03:09 -0300

    criei uma pagina de filme

--- cine-orc/src/pages/Movie/movie.jsx
+++ cine-orc/src/pages/Movie/movie.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './movie.css'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import LogoFilme from '../../components/LogoFIlme';
+import Header from '../../components/Header'
+
+const apiKey = import.meta.env.VITE_API_KEY
+const moviesURL = import.meta.env.VITE_API
 
 function Movie() {
 
     const [filme, setFilme] = useState('');
 
+    const {id} = useParams()
+    const [movie, setMovie] = useState(null)
 
+    const getMovie = async(url) =>{
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setMovie(data)
+    }
+
+    useEffect(() => {
+        const movieUrl = `${moviesURL}${id}?${apiKey}`
+        getMovie(movieUrl)
+    },[])
 
     return (
-        <div>
-            <h1>a</h1>
+        <div className='paginaF'>
+            {/* <Header/> */}
+            {movie && <>
+            <LogoFilme movie={movie} showLink={false}/>
+            </>}
+            <div className='descricao'>
+                <h3>Descricao</h3>
+                <p>{movie.overview}</p>
+            </div>
         </div>
     )
 }
