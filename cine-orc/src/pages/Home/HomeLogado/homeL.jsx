c17ae833f63bca71eec595ff43199a2b7c7e820c
Galdino <pedro.wollmann@gmail.com>
2024-12-02 22:47:48 -0300

    cinema adicionadp

--- cine-orc/src/pages/Home/HomeLogado/homeL.jsx
+++ cine-orc/src/pages/Home/HomeLogado/homeL.jsx
@@ -14,6 +14,7 @@ function HomeL() {
     const [filme, setFilme] = useState('');
     const [topMovies, setTopMovies] = useState([]);
     const [pesquisados, setPesquisados] = useState([]);
+    const [cinema, setCinema] = useState([])
 
     const getTopRatedMovies = async (url) => {
         const res = await fetch(url);
@@ -21,6 +22,16 @@ function HomeL() {
         setTopMovies(data.results);
         setPesquisados(data.results); 
     };
+    const getCinema = async (url) =>{
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data)
+        setCinema(data.results)
+    }
+    useEffect(() =>{
+        const cinemaUrl = `${moviesURL}now_playing?${apiKey}&language=${language}`;
+        getCinema(cinemaUrl)
+    },[])
 
     useEffect(() => {
         const topRatedUrl = `${moviesURL}top_rated?${apiKey}&${language}`;
@@ -49,10 +60,15 @@ function HomeL() {
             <Header logado={logado} username={nome} />
             <SearchBar onSearch={handleSearch} /> 
             <Filter />
-            <div className='filmes'>
-                {pesquisados && pesquisados.slice(0, 9).map((movie) => (
-                    <LogoFilme key={movie.id} movie={movie} />
-                ))}
+            <div className='container-filmes'>
+                <h2 className='categoria'>Maior nota</h2>
+                <div className='filmes'>
+                    {pesquisados && pesquisados.slice(0,9).map((movie) => <LogoFilme key={movie.id} movie={movie} />)}
+                </div>
+                <h2 className='categoria'> Em Cartaz</h2>
+                <div className='filmes'>
+                    {cinema && cinema.slice(0,9).map((movie) => <LogoFilme key={movie.id} movie={movie} />)}
+                </div>
             </div>
         </div>
     );
