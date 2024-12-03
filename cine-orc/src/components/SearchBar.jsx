e38c795cd763bc904a1e12d9e5eae53a82b8ac1a
Guilherme <guilorenziventura@gmail.com>
2024-12-02 21:38:44 -0300

    fiz a logica da search bar

--- cine-orc/src/components/SearchBar.jsx
+++ cine-orc/src/components/SearchBar.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import './SearchBar.css'
-function SearchBar(pesquisar) {
+import React, { useState } from 'react';
+import './SearchBar.css';
+
+function SearchBar({ onSearch }) {
     const [filme, setFilme] = useState('');
-  return (
-    <div className='search-bar'>
-      <form >
-        <input
-            type='search'
-            placeholder='Digite o nome do filme'
-            value={filme}
-            onChange={(e) => setFilme(e.target.value)} //ver com o guilerme oq isso significa
-        ></input>
-      </form>
-    </div>
-  )
+    
+    const handleChange = (e) => {
+        setFilme(e.target.value);
+        onSearch(e.target.value); 
+    };
+
+    return (
+        <div className='search-bar'>
+            <form>
+                <input
+                    type='search'
+                    placeholder='Digite o nome do filme'
+                    value={filme}
+                    onChange={handleChange} 
+                ></input>
+            </form>
+        </div>
+    );
 }
 
-export default SearchBar
+export default SearchBar;
--- cine-orc/src/pages/Home/home.jsx
+++ cine-orc/src/pages/Home/home.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
 import Filter from '../../components/Filter';
@@ -12,36 +12,53 @@ const moviesURL = import.meta.env.VITE_API
 function Home() {
 
     const [filme, setFilme] = useState('');
-    
+    const [pesquisados, setPesquisados] = useState('');
+    const [topMovies, setTopMovies] = useState([]);
     const handleHomeRedirect = () => {
         navigate('/');
     };
-    const [topMovies, setTopMovies] = useState([])
-    const getTopRatedMovies = async(url) =>{
-    const res = await fetch(url)
-    const data = await res.json()
-    setTopMovies(data.results)
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+        setTopMovies(data.results)
+        setPesquisados(data.results)
     }
+
+    const handleSearch = (pesquisa) => {
+        setFilme(pesquisa);
+        if (pesquisa === ''){
+            setPesquisados(topMovies);
+        }
+        else{
+            const filtrados = topMovies.filter((movie) => 
+                movie.title.toLowerCase().includes(pesquisa.toLowerCase())
+            );
+            setPesquisados(filtrados);
+        }
+    }
+
     useEffect(() => {
         const topRatedUrl = `${moviesURL}top_rated?${apiKey}&${language}`
         getTopRatedMovies(topRatedUrl)
-    },[])
+    }, [])
+
     return (
         <div>
-            <Header redirect={handleHomeRedirect}/>
-            <div className='search-bar'>
+            <Header redirect={handleHomeRedirect} />
+            <SearchBar onSearch={handleSearch}/>
+            {/* <div className='search-bar'>
                 <input
                     type='search'
                     placeholder='Digite o nome do filme'
                     value={filme}
                     onChange={(e) => setFilme(e.target.value)}
                 ></input>
-            </div>
-            <Filter/>
+            </div> */}
+            <Filter />
             <div className='container-filmes'>
                 <h2 className='categoria'>Melhores Filmes</h2>
                 <div className='filmes'>
-                    {topMovies && topMovies.map((movie)=><LogoFilme key={movie.id} movie={movie}/>)}
+                    {pesquisados && pesquisados.map((movie) => <LogoFilme key={movie.id} movie={movie} />)}
                 </div>
             </div>
         </div>
